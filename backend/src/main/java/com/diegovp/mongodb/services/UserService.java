e86299a2504f9ea4bf6be2c11b7bfee331875b55
DiegoVP66 <dvpdev33@gmail.com>
2022-03-07 14:46:09 -0300

    PUT update user

--- backend/src/main/java/com/diegovp/mongodb/services/UserService.java
+++ backend/src/main/java/com/diegovp/mongodb/services/UserService.java
@@ -24,8 +24,7 @@ public class UserService {
 	}
 
 	public UserDTO findById(String id) {
-		Optional<User> result = repository.findById(id);
-		User entity = result.orElseThrow(() -> new ResourceNotFoundException("Resource not found!"));
+		User entity = getEntityById(id);
 		return new UserDTO(entity);
 	}
 
@@ -34,6 +33,19 @@ public class UserService {
 		copyToDTO(entity, dto);
 		entity = repository.save(entity);
 		return new UserDTO(entity);
+	}
+
+	public UserDTO update(String id, UserDTO dto) {
+		User entity = getEntityById(id);
+		copyToDTO(entity, dto);
+		entity = repository.save(entity);
+		return new UserDTO(entity);
+
+	}
+
+	private User getEntityById(String id) {
+		Optional<User> result = repository.findById(id);
+		return result.orElseThrow(() -> new ResourceNotFoundException("Resource not found!"));
 	}
 
 	private void copyToDTO(User entity, UserDTO dto) {
