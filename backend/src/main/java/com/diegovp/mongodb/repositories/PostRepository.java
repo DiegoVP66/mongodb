edb0bc16a715fdd7e45a0fb1e4d74de4084e8d56
DiegoVP66 <dvpdev33@gmail.com>
2022-03-07 16:28:33 -0300

    GET Post by title, query operators

--- backend/src/main/java/com/diegovp/mongodb/repositories/PostRepository.java
+++ backend/src/main/java/com/diegovp/mongodb/repositories/PostRepository.java
@@ -3,6 +3,7 @@ package com.diegovp.mongodb.repositories;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.diegovp.mongodb.models.entities.Post;
@@ -11,4 +12,9 @@ import com.diegovp.mongodb.models.entities.Post;
 public interface PostRepository extends MongoRepository<Post, String> {
 	
 	List<Post> findByTitleContainingIgnoreCase(String text);
+	
+	@Query("{'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
+	
+	
 }
