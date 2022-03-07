28bba09406ddff41fd2c040d9d73022be03000d1
DiegoVP66 <dvpdev33@gmail.com>
2022-03-07 14:09:51 -0300

    GET user by id

--- backend/src/main/java/com/diegovp/mongodb/services/UserService.java
+++ backend/src/main/java/com/diegovp/mongodb/services/UserService.java
@@ -1,6 +1,7 @@
 package com.diegovp.mongodb.services;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,7 @@ import org.springframework.stereotype.Service;
 import com.diegovp.mongodb.models.dto.UserDTO;
 import com.diegovp.mongodb.models.entities.User;
 import com.diegovp.mongodb.repositories.UserRepository;
+import com.diegovp.mongodb.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -19,5 +21,11 @@ public class UserService {
 	public List<UserDTO> findAll() {
 		List<User> list = repository.findAll();
 		return list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+	}
+
+	public UserDTO findById(String id) {
+		Optional<User> result = repository.findById(id);
+		User entity = result.orElseThrow(() -> new ResourceNotFoundException("Resource not found!"));
+		return new UserDTO(entity);
 	}
 }
